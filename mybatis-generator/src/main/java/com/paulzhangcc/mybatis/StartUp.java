ad88093f403d5fc4097e0dd2bb882d5ae5743fc6
zhangjinfeng <zhangjinfeng@eagle-realestate.com>
2017-11-20 15:27:53 +0800

    init

--- mybatis-generator/src/main/java/com/paulzhangcc/mybatis/StartUp.java
+++ mybatis-generator/src/main/java/com/paulzhangcc/mybatis/StartUp.java
@@ -10,12 +10,16 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 public class StartUp {
+    /**
+     * 读取resource目录下的配置文件
+     */
+    public static final String CONFIG = "generatorConfig.xml";
     public static void main(String[] args) throws URISyntaxException {
         try {
             List<String> warnings = new ArrayList<String>();
             boolean overwrite = true;
             ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            InputStream is = classloader.getResourceAsStream(CONFIG);
             ConfigurationParser cp = new ConfigurationParser(warnings);
             Configuration config = cp.parseConfiguration(is);
             DefaultShellCallback callback = new DefaultShellCallback(overwrite);
